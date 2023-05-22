227d0c76bab614da6a2130b1ea9e283995c55dea
Mateusz Gula <mateuszgua@wp.eu>
2023-05-22 18:26:18 +0200

    Feat: add s3 bucket connection, add tests for s3 bucket connection

--- app/config.py
+++ app/config.py
@@ -7,8 +7,7 @@ load_dotenv(path.join(basedir, '.env'))
 
 class Config:
 
-    def load_env_variables():
-        AWS_KEY_ID = os.getenv('AWS_KEY_ID')
-        AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
-        AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
-        AWS_BUCKET = os.getenv('AWS_BUCKET')
+    AWS_KEY_ID = os.getenv('AWS_KEY_ID')
+    AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+    AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
+    AWS_BUCKET = os.getenv('AWS_BUCKET')
