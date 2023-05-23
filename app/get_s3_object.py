b884287086b1d6839cb7cd66e38416c270dbf80f
Mateusz Gula <mateuszgua@wp.eu>
2023-05-23 13:04:01 +0200

    Feat: change to return object from s3 and create spark df from other df from pandas

--- app/get_s3_object.py
+++ app/get_s3_object.py
@@ -5,9 +5,9 @@ from config import Config
 def get_object_from_s3(file_path):
     try:
         s3 = get_s3_client()
-        obj = s3.get_object(Bucket=Config.AWS_BUCKET,
+        s3_object = s3.get_object(Bucket=Config.AWS_BUCKET,
                             Key=file_path)
     except:
         print(f"Error with get list from s3 bucket")
     else:
-        return obj
+        return s3_object
--- app/get_spark_df.py
+++ app/get_spark_df.py
@@ -1,15 +1,20 @@
+import io
+
 from pyspark.sql import SparkSession
+
 from config import Config
 
 
 config = Config()
 
 
-def get_df_spark(header, data):
+def get_df_spark(pandas_df):
     try:
-        spark = SparkSession.builder.appName('sparkdf').getOrCreate()
-        dataframe = spark.createDataFrame(data, header)
-    except:
-        print(f"Error with create dataframe")
+        spark = SparkSession.builder.appName('Read Parquet from Bytes').getOrCreate()
+        spark_df = spark.createDataFrame(pandas_df)
+        spark.stop()
+    except Exception as e:
+        print(f"Error with create dataframe: {e}")
     else:
-        return dataframe
+        return spark_df
+    
